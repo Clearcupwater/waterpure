bb00c7fd9725a74406c313e1a64530a7b39b622a
Old <old@forgetlooks.com>
2019-10-09 14:06:34 -0700

    adding items to list maker...

--- app/src/main/java/com/example/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -1,20 +1,27 @@
 package com.example.listmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>(){
     val listTiles = arrayOf("Shopping List", "Chores", "Andriod Tutorials")
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //setting up the layout...
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder,parent,false)
+        return ListSelectionViewHolder(view)
     }
 
     override fun getItemCount(): Int {
+        //setting up the amount of items on the list view
        return listTiles.size
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.listPosition.text = (position +1).toString()
+        holder.listTitle.text = listTiles[position]
     }
 
 
